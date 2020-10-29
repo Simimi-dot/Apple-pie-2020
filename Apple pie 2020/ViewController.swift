d39442360a7ad1f3f0d5b494ff1b0f10828bc1ac
Egor Astakhov <noise718@gmail.com>
2020-10-29 16:23:03 +0300

    Implemented correct word label update.

--- Apple pie 2020/ViewController.swift
+++ Apple pie 2020/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     // MARK: - IB Outlets
     @IBOutlet weak var treeImageView: UIImageView!
     @IBOutlet var letterButtons: [UIButton]!
-    @IBOutlet weak var coorectWordLabel: UILabel!
+    @IBOutlet weak var correctWordLabel: UILabel!
     @IBOutlet weak var scoreLabel: UILabel!
     
     
@@ -115,11 +115,22 @@ class ViewController: UIViewController {
         updateUI()
     }
     
+    func updateCorrectWordLabel() {
+        var displayWord = [String]()
+        for letter in currentGame.guessedWord{
+            displayWord.append(String(letter))
+        }
+//        correctWordLabel.text = currentGame.guessedWord
+        correctWordLabel.text = displayWord.joined(separator: " ")
+    }
+    
     func updateUI(){
         let movesRemaining = currentGame.incorrectMovesRemaining
-        let image = "Tree\(movesRemaining < 8 ? movesRemaining : 7)"
+//        let imageNumber = movesRemaining < 0 ? 0 : movesRemaining < 8 ? movesRemaining : 7
+        let imageNumber = (movesRemaining + 64) % 8
+        let image = "Tree\(imageNumber)"
         treeImageView.image = UIImage(named: image)
-        
+        updateCorrectWordLabel()
         scoreLabel.text = "Выигрыши: \(totalWins), Проигрыши: \(totalLosses)"
     }
     
@@ -130,10 +141,13 @@ class ViewController: UIViewController {
     
     
     
-    // MARK: -IB Actions
+    // MARK: - IB Actions
     
     @IBAction func letterButtonPressed(_ sender: UIButton) {
         sender.isEnabled = false
+        let letter = sender.title(for: .normal)!
+        currentGame.playerGuessed(letter: Character(letter))
+        updateUI()
     }
     
 }
