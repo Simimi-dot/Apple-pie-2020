b53a7426b2563c1dd2bc52ba0d043dfbc5798a2f
Egor Astakhov <noise718@gmail.com>
2020-10-29 11:53:27 +0300

    Initial Commit

---
+++ Apple pie 2020/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple pie 2020
+//
+//  Created by Егор Астахов on 29.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
