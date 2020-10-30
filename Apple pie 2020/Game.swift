0d7ea7adcfe8edd03aa8d355a1ee43f5b808c4b5
Egor Astakhov <noise718@gmail.com>
2020-10-30 12:52:26 +0300

    Added game state.

--- Apple pie 2020/Game.swift
+++ Apple pie 2020/Game.swift
@@ -19,9 +19,8 @@ struct Game {
     
     var guessedWord: String{
         var wordToShow = ""
-        
         for letter in word{
-            if guessedLetters.contains(Character (letter.lowercased())){
+            if guessedLetters.contains(Character (letter.lowercased())) || letter == "-" || letter == " " {
                 wordToShow += String(letter)
             }else{
                 wordToShow += "_"
